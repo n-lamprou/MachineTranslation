7c620321a05bbb26b713e525a1f03f70756ca128
Nik Lamprou <niklamprou@TCR-C02WJ093HV2P.local>
2018-11-29 09:54:20 +0000

    feat: Add text preprocessing functions
    Functions to pad and tokenize text

---
+++ text_preprocessing.py
@@ -0,0 +1,40 @@
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+def tokenize(x):
+    """
+    Tokenize x
+    :param x: List of sentences/strings to be tokenized
+    :return: Tuple of (tokenized x data, tokenizer used to tokenize x)
+    """
+    keras_tokenizer = Tokenizer()
+    keras_tokenizer.fit_on_texts(x)
+    return keras_tokenizer.texts_to_sequences(x), keras_tokenizer
+
+def pad(x, length=None):
+    """
+    Pad x
+    :param x: List of sequences.
+    :param length: Length to pad the sequence to.  If None, use length of longest sequence in x.
+    :return: Padded numpy array of sequences
+    """
+    padded_x = pad_sequences(x, maxlen=length, padding='post')
+    return padded_x
+
+def preprocess(x, y):
+    """
+    Preprocess x and y
+    :param x: Feature List of sentences
+    :param y: Label List of sentences
+    :return: Tuple of (Preprocessed x, Preprocessed y, x tokenizer, y tokenizer)
+    """
+    preprocess_x, x_tk = tokenize(x)
+    preprocess_y, y_tk = tokenize(y)
+
+    preprocess_x = pad(preprocess_x)
+    preprocess_y = pad(preprocess_y)
+
+    # Keras's sparse_categorical_crossentropy function requires the labels to be in 3 dimensions
+    preprocess_y = preprocess_y.reshape(*preprocess_y.shape, 1)
+
+    return preprocess_x, preprocess_y, x_tk, y_tk
